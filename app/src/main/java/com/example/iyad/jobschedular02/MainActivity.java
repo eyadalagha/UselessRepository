afc6f7602bf3e981ab879b8d63b09cbfc22a13c4
eyadalagha <ialagha@iugaza.edu.ps>
2015-08-16 11:12:14 +0300

    Added some more useless code

--- app/src/main/java/com/example/iyad/jobschedular02/MainActivity.java
+++ app/src/main/java/com/example/iyad/jobschedular02/MainActivity.java
@@ -22,7 +22,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
+        //
         Button btn = (Button) this.findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/iyad/jobschedular02/TestClass.java
@@ -0,0 +1,7 @@
+package com.example.iyad.jobschedular02;
+
+/**
+ * Created by iyad on 8/16/2015.
+ */
+public class TestClass {
+}
